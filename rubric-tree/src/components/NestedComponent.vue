c5fb14e8e8abda4bd20743cd9191f777562fd104
Vadim Pankov <vp_pankov@mail.ru>
2024-11-27 14:40:46 +0300

    fix

--- rubric-tree/src/components/NestedComponent.vue
+++ rubric-tree/src/components/NestedComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-for="(item, index) in props.items" :key="index + new Date().getTime()" class="ml-10">
     <div class="flex items-center">
-      <div v-if="item.children.length" @click="item.showSection = !item.showSection"
+      <div v-if="isShowChevron(item)" @click="item.showSection = !item.showSection"
            class="rounded-full hover:bg-slate-500 size-5 flex justify-center items-center"
            :class="{'rotate-90': item.showSection}"
       >
@@ -37,5 +37,8 @@ const changeData = (count, value) => {
   setTimeout(() => {
     emit('changeData', count, value)
   },50)
+}
+const isShowChevron = (item) => {
+  return item.children && item.children.length
 }
 </script>
